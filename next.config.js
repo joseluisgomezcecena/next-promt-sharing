a4e2b9cac4e6a43d7d1d8a4b3ff23b9fe5b4b4c0
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-05-18 14:24:32 -0700

    Getting all data with getServerSideProps() in index.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    API_URL: "http://localhost:8000/api", //django api.
+    API_URL: "http://127.0.0.1:8000", //django api.
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,23 @@ import styles from '@/styles/Home.module.css'
 import Layout from '../components/layout/Layout'
 import Home from "@/components/Home";
 const inter = Inter({ subsets: ['latin'] })
-
-export default function Index() {
+import axios from "axios";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+export default function Index({data}) {
+  console.log('jobs', data);
   return (
       <Layout>
         <Home />
       </Layout>
   )
+}
+
+export async function getServerSideProps(){
+    const response = await axios.get(`${process.env.API_URL}/api/jobs`)
+    const data = response.data
+    return {
+        props: {
+            data,
+        }
+    }
 }
